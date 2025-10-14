225817e3acda0bb00b0182ca59526a4e30e6bfbe
Aditya Raj Singh <ranawataaditya06@gmail.com>
2025-10-14 19:23:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
+let slider = document.querySelector('.slider');
+
+next.addEventListener('click', function(){
+    let slides = document.querySelectorAll('.slides');
+    slider.appendChild(slides[0]);
+})
+prev.addEventListener('click', function(){
+    let slides = document.querySelectorAll('.slides');
+    slider.prepend(slides[slides.length - 1]);
+})
+
+const menu = document.querySelector("#menu");
+const nav = document.querySelector(".links");
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+}
